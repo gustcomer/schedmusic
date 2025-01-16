9cc365af4ff628d3fea49233b421c7bb5c1ea9ca
gustcomer <gustcomer@gmail.com>
2025-01-16 18:29:06 -0300

    feat: add Artist id specific metadata with generateMetadata()

--- src/app/(content)/artist/[artistId]/page.tsx
+++ src/app/(content)/artist/[artistId]/page.tsx
@@ -1,8 +1,22 @@
+import {Metadata} from "next";
+
+type Props = {
+  params: Promise<{ artistId: string }>;
+};
+
+export const generateMetadata = async ({
+  params
+}: Props): Promise<Metadata> => {
+  const id = (await params).artistId;
+  return {
+    title: `Artist ${id}`,
+  }
+}
+
 export default async function ArtistDetails({
   params
-}: {
-  params: Promise<{ artistId: string }>;
-}) {
+}: Props
+) {
   const artistId = (await params).artistId;
   console.log(artistId);
   return (<h1>Details about artist {artistId}</h1>);
