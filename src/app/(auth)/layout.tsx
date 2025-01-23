07cba2b76f5a8f9b0958477f73e6d17bc950d38f
gustcomer <gustcomer@gmail.com>
2025-01-23 17:57:56 -0300

    feat: render active link in bold. add tailwind support.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -23,8 +23,9 @@ export default function AuthLayout({
           padding: "0.5rem"
         }}>
         {navLinks.map(link => {
+          const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/");
           return (
-            <Link href={link.href} key={link.name}>
+            <Link className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4" } href={link.href} key={link.name}>
               {link.name}
             </Link>
           );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 
+import "./styles.css";
+
 export const metadata: Metadata = {
   title: {
     default: "SchedMusic - Your Music Scheduler",
