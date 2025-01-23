d0108740eb49a80a3d475c7d586cd30de7402316
gustcomer <gustcomer@gmail.com>
2025-01-23 16:12:44 -0300

    refactor: add common layout to auth and content. Use usePathname in auth and transform auth into client components.

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -15,25 +15,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        <header
-          style={{
-            backgroundColor: "lightblue",
-            padding: "1rem"
-          }}>
-          <p>Header</p>
-        </header>
-        {children}
-        <footer
-          style={{
-            backgroundColor: "ghostwhite",
-            padding: "1rem"
-          }}
+    <>
+      <header
+        style={{
+          backgroundColor: "lightblue",
+          padding: "1rem"
+        }}>
+        <p>Header</p>
+      </header>
+      {children}
+      <footer
+        style={{
+          backgroundColor: "ghostwhite",
+          padding: "1rem"
+        }}
         >
-          <p>Footer</p>
-        </footer>
-      </body>
-    </html>
+        <p>Footer</p>
+      </footer>
+    </>
   )
 }
