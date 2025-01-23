984b3388671a975e87186481749a668933068dac
gustcomer <gustcomer@gmail.com>
2025-01-23 10:29:05 -0300

    feat: link home to about page and vice versa

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export const metadata = {
   title: 'SchedMusic | About Page'
 }
 
 export default function About() {
-  return <h1>About page</h1>
+  return (
+    <>
+      <h1>About page</h1>
+      <Link href="/">Home</Link>
+    </>
+  );
 }
--- src/app/(content)/artist/page.tsx
+++ src/app/(content)/artist/page.tsx
@@ -1,16 +1,19 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Artists"
 }
 
 export default function ArtistList() {
+  const artistId = 10;
   return (
     <>
       <h1>Artist List</h1>
-      <h2>Artist 1</h2>
-      <h2>Artist 2</h2>
-      <h2>Artist 3</h2>
+      <h2><Link href="/artist/1">Artist 1</Link></h2>
+      <h2><Link href="/artist/2">Artist 2</Link></h2>
+      <h2><Link href="/artist/3" replace>Artist 3</Link></h2>
+      <h2><Link href={`/artist/${artistId}`}>Artist {artistId}</Link></h2>
     </>
   )
 }
