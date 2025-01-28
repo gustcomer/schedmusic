27509a2499e5a5ced980a7e604663a2598c502c2
gustcomer <gustcomer@gmail.com>
2025-01-28 08:52:54 -0300

    feature: throw error at /artist/id/album/id 33% of the time

--- src/app/(content)/artist/[artistId]/album/[albumId]/page.tsx
+++ src/app/(content)/artist/[artistId]/album/[albumId]/page.tsx
@@ -1,10 +1,18 @@
 import { redirect } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count );
+}
+
 export default async function ArtistAlbumDetail({
   params,
 }: {
   params: Promise<{ artistId: string; albumId: string }>;
 }) {
+  const random = getRandomInt(3);
+  if(random === 1) {
+    throw new Error("Error loading review");
+  }
   const { artistId, albumId } = await params;
   if (parseInt(artistId) > 1000) {
     //notFound(); // let's just try another option with redirect
