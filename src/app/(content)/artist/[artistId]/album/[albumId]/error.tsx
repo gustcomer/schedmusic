b55b4e156d0978a2069186410da6e9f051158e42
gustcomer <gustcomer@gmail.com>
2025-01-29 10:48:34 -0300

    feat: add error recovery in the server (with bug)

--- src/app/(content)/artist/[artistId]/album/[albumId]/error.tsx
+++ src/app/(content)/artist/[artistId]/album/[albumId]/error.tsx
@@ -1,7 +1,26 @@
 "use client";
 
-export default function ErrorBoundary({error}: {
-  error: Error
+import { useRouter } from "next/navigation";
+import {startTransition} from "react";
+
+export default function ErrorBoundary({
+  error,
+  reset
+}: {
+  error: Error,
+  reset: () => void
 }) {
-  return <div>{error.message}</div>
+  const router = useRouter();
+
+  const reload = () => {
+      router.refresh();
+      reset();
+  };
+
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={() => startTransition(reload)}>Try again</button>
+    </div>
+  );
 }
